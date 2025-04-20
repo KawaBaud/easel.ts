52f9121e20c3fa6adcde4dd28f2a71b17340ef0b
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-04-20 12:28:42 +0300

    impl global constant

---
+++ src/MUSI.js
@@ -0,0 +1,55 @@
+import { createPerspCamera } from "./cameras/PerspCamera.js";
+import { createMaterial } from "./materials/Material.js";
+import { createEuler } from "./maths/Euler.js";
+import { MathsUtils } from "./maths/MathsUtils.js";
+import { createMatrix4 } from "./maths/Matrix4.js";
+import { createQuaternion } from "./maths/Quaternion.js";
+import { createSphere } from "./maths/Sphere.js";
+import { createVector3 } from "./maths/Vector3.js";
+import { createMesh } from "./objects/Mesh.js";
+import { createObject3D } from "./objects/Object3D.js";
+import { createCanvasRenderer } from "./renderers/canvas/CanvasRenderer.js";
+import { createScene } from "./scenes/Scene.js";
+import { createCubeShape } from "./shapes/CubeShape.js";
+import { createShape } from "./shapes/Shape.js";
+
+let instance = null;
+
+/**
+ * @returns {Object}
+ */
+function _createMUSI() {
+    if (instance) return instance;
+    instance = Object.freeze({
+        /* cameras */
+        PerspCamera: createPerspCamera,
+
+        /* materials */
+        Material: createMaterial,
+
+        /* maths */
+        Vector3: createVector3,
+        Matrix4: createMatrix4,
+        Quaternion: createQuaternion,
+        Euler: createEuler,
+        Sphere: createSphere,
+        Maths: MathsUtils,
+
+        /* objects */
+        Mesh: createMesh,
+        Object3D: createObject3D,
+
+        /* renderers */
+        CanvasRenderer: createCanvasRenderer,
+
+        /* scenes */
+        Scene: createScene,
+
+        /* shapes */
+        Shape: createShape,
+        CubeShape: createCubeShape,
+    });
+    return instance;
+}
+
+export const MUSI = _createMUSI();
