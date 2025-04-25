cd27121365f5f8e599b6600bdc76d19b3a4b561c
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-04-25 20:44:24 +0300

    impl viewport and scissor like 3js

--- src/renderers/common/RenderTarget.js
+++ src/renderers/common/RenderTarget.js
@@ -1,10 +1,15 @@
 import { MIN_LOGICAL_HEIGHT, MIN_LOGICAL_WIDTH } from "../../constants.js";
+import { createVector4 } from "../../maths/Vector4.js";
 
 /**
  * @typedef {Object} RenderTarget
  * @property {number} width
  * @property {number} height
  * @property {number} aspectRatio
+ * @property {Vector4} viewport
+ * @property {Vector4} scissor
+ * @property {boolean} scissorTest
+ * @property {boolean} isRenderTarget
  */
 
 /**
@@ -14,6 +19,9 @@ import { MIN_LOGICAL_HEIGHT, MIN_LOGICAL_WIDTH } from "../../constants.js";
 export function createRenderTarget(options = {}) {
     const _width = options.width ?? MIN_LOGICAL_WIDTH;
     const _height = options.height ?? MIN_LOGICAL_HEIGHT;
+
+    const _viewport = createVector4(0, 0, _width, _height);
+    const _scissor = createVector4(0, 0, _width, _height);
 
     const _target = {
         /**
@@ -33,11 +41,41 @@ export function createRenderTarget(options = {}) {
         aspectRatio: _width / _height,
 
         /**
+         * @type {Vector4}
+         */
+        viewport: _viewport,
+
+        /**
+         * @type {Vector4}
+         */
+        scissor: _scissor,
+
+        /**
+         * @type {boolean}
+         * @default false
+         */
+        scissorTest: false,
+
+        /**
          * @type {boolean}
          * @readonly
          * @default true
          */
         isRenderTarget: true,
+
+        /**
+         * @param {number} width
+         * @param {number} height
+         * @returns {RenderTarget}
+         */
+        setSize(width, height) {
+            _target.width = width;
+            _target.height = height;
+            _target.aspectRatio = width / height;
+            _viewport.set(0, 0, width, height);
+            _scissor.set(0, 0, width, height);
+            return _target;
+        },
     };
 
     return _target;
