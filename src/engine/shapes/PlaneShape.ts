40742eb462e4c025db7402cbadea6f6b76e3cccc
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-03 23:27:32 +0300

    initial commit

---
+++ src/engine/shapes/PlaneShape.ts
@@ -0,0 +1,33 @@
+import { Vector3 } from "../maths/Vector3.ts";
+import { Shape } from "./Shape.ts";
+
+export class PlaneShape extends Shape {
+	readonly isPlaneShape = true;
+
+	constructor(width = 1, height = 1) {
+		super();
+
+		const halfWidth = width / 2;
+		const halfHeight = height / 2;
+
+		this.vertices = [
+			new Vector3(-halfWidth, 0, -halfHeight),
+			new Vector3(halfWidth, 0, -halfHeight),
+			new Vector3(halfWidth, 0, halfHeight),
+			new Vector3(-halfWidth, 0, halfHeight),
+		];
+
+		this.indices = [
+			0,
+			1,
+			2,
+			0,
+			2,
+			3,
+		];
+	}
+
+	override clone(): PlaneShape {
+		return new PlaneShape().copy(this);
+	}
+}
