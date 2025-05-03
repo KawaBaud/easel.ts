e6ec5fd09961d6d91934d198c0a782ecc2eb44c5
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-03 23:26:46 +0300

    JS->TS

---
+++ src/engine/objects/Mesh.ts
@@ -0,0 +1,28 @@
+import type { Material } from "../materials/Material.ts";
+import type { Shape } from "../shapes/Shape.ts";
+import { Object3D } from "./Object3D.ts";
+
+export class Mesh extends Object3D {
+	readonly isMesh = true;
+
+	constructor(
+		public shape: Shape,
+		public material: Material,
+	) {
+		super();
+		this.updateMatrix();
+	}
+
+	override clone(): Mesh {
+		return new Mesh(this.shape.clone(), this.material.clone()).copy(this);
+	}
+
+	override copy(source: Mesh): this {
+		super.copy(source);
+
+		this.shape = source.shape.clone();
+		this.material = source.material.clone();
+
+		return this;
+	}
+}
