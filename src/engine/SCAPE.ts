8487f89e396b49c2ee5a0a7991621a22e0d12f74
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-03 23:26:00 +0300

    more exports

--- src/engine/SCAPE.ts
+++ src/engine/SCAPE.ts
@@ -1,21 +1,27 @@
 import { Camera } from "./cameras/Camera.ts";
-import { IsometricCamera } from "./cameras/IsometricCamera.ts";
 import { OrthoCamera } from "./cameras/OrthoCamera.ts";
+import { Material } from "./materials/Material.ts";
 import { Euler } from "./maths/Euler.ts";
 import { MathUtils } from "./maths/MathUtils.ts";
 import { Matrix4 } from "./maths/Matrix4.ts";
 import { Quaternion } from "./maths/Quaternion.ts";
 import { Vector2 } from "./maths/Vector2.ts";
 import { Vector3 } from "./maths/Vector3.ts";
-import { Object2D } from "./objects/Object2D.ts";
+import { Mesh } from "./objects/Mesh.ts";
+import { Object3D } from "./objects/Object3D.ts";
 import { CanvasRenderer } from "./renderers/CanvasRenderer.ts";
 import { Scene } from "./scenes/Scene.ts";
+import { CubeShape } from "./shapes/CubeShape.ts";
+import { PlaneShape } from "./shapes/PlaneShape.ts";
+import { Shape } from "./shapes/Shape.ts";
 
 export const SCAPE = {
 	// cameras
 	Camera,
-	IsometricCamera,
 	OrthoCamera,
+
+	// materials
+	Material,
 
 	// maths
 	Math: MathUtils,
@@ -26,11 +32,17 @@ export const SCAPE = {
 	Euler,
 
 	// objects
-	Object2D,
+	Object3D,
+	Mesh,
 
 	// renderers
 	CanvasRenderer,
 
 	// scenes
 	Scene,
+
+	// shapes
+	Shape,
+	CubeShape,
+	PlaneShape,
 };
