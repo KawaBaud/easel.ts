044d5f10a88fa054eae0154753c3260cd90102eb
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-03 23:26:35 +0300

    JS->TS

---
+++ src/engine/materials/Material.ts
@@ -0,0 +1,28 @@
+import type { Cloneable, Copyable } from "../types/interfaces.ts";
+
+export interface MaterialOptions {
+	color?: number;
+	wireframe?: boolean;
+}
+
+export class Material implements Cloneable<Material>, Copyable<Material> {
+	readonly isMaterial = true;
+
+	color: number;
+	wireframe: boolean;
+
+	constructor(options: MaterialOptions = {}) {
+		this.color = options.color ?? 0xFFFFFF;
+		this.wireframe = options.wireframe ?? false;
+	}
+
+	clone(): Material {
+		return new Material().copy(this);
+	}
+
+	copy(source: Material): this {
+		this.color = source.color;
+		this.wireframe = source.wireframe;
+		return this;
+	}
+}
