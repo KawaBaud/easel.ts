6968cb105b0e55c9ec0dac37f2c3987f1543302d
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 10:46:54 +0300

    remove iter

--- src/engine/types/interfaces.ts
+++ src/engine/types/interfaces.ts
@@ -10,10 +10,6 @@ export interface Equatable<T> {
 	equals(other: T): boolean;
 }
 
-export interface Iterable<T> {
-	[Symbol.iterator](): IterableIterator<T>;
-}
-
 export interface Serializable<T = number[]> {
 	fromArray(array: T, offset?: number): this;
 	toArray(array?: T, offset?: number): T;
