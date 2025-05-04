c69801192e58338e9d34b25b2198090baa45dabc
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 22:18:17 +0300

    extension

--- src/renderers/common/RenderPipeline.ts
+++ src/renderers/common/RenderPipeline.ts
@@ -1,3 +1,29 @@
+import type { Camera } from "../../cameras/Camera.ts";
+import type { Scene } from "../../scenes/Scene.ts";
 import { Pipeline } from "./Pipeline.ts";
+import type { Rasterizer } from "./Rasterizer.ts";
+import { RenderTarget } from "./RenderTarget.ts";
 
-export class RenderPipeline extends Pipeline {}
+export class RenderPipeline extends Pipeline {
+	renderTarget: RenderTarget;
+
+	constructor(width?: number, height?: number) {
+		super();
+		this.renderTarget = new RenderTarget(width, height);
+	}
+
+	override render(scene: Scene, camera: Camera, rasterizer: Rasterizer): this {
+		camera.updateMatrixWorld();
+
+		this.cull(scene, camera);
+
+		super.render(scene, camera, rasterizer);
+
+		return this;
+	}
+
+	setSize(width: number, height: number): this {
+		this.renderTarget.setSize(width, height);
+		return this;
+	}
+}
