dd3562589284bc9a99788ed497fab4aeb1329b77
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 13:52:07 +0300

    use Color

--- src/engine/renderers/Rasterizer.ts
+++ src/engine/renderers/Rasterizer.ts
@@ -1,33 +1,30 @@
+import type { Color } from "../core/Color.ts";
 import type { Vector3 } from "../maths/Vector3.ts";
 
 export abstract class Rasterizer {
-	readonly isRasterizer = true;
+	readonly isRasterizer: boolean = true;
 
 	abstract beginFrame(): void;
-	abstract clear(color?: string | number): void;
+	abstract clear(color?: Color): void;
+
 	abstract drawLine(
 		x0: number,
 		y0: number,
 		x1: number,
 		y1: number,
-		color?: string | number,
+		color?: Color,
 	): void;
 	abstract drawPixel(
 		x: number,
 		y: number,
-		color?: string | number,
+		color?: Color,
 	): void;
 	abstract drawTriangle(
 		v1: Vector3,
 		v2: Vector3,
 		v3: Vector3,
-		color?: string | number,
+		color?: Color,
 	): void;
-	abstract drawTriangleFilled(
-		v1: Vector3,
-		v2: Vector3,
-		v3: Vector3,
-		color?: string | number,
-	): void;
+
 	abstract endFrame(): void;
 }
