53aece17982ab03d46fbfdc7a461ccce15e3a1c1
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 22:04:13 +0300

    safety

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,3 @@
-export function getArray<T extends ArrayLike<unknown>>(
-	array: T,
-	i: number,
-): T[number] | 0 {
-	return array[i] ?? 0;
-}
-
-export function setArray<T extends Array<unknown>>(
-	array: T,
-	i: number,
-	value: T[number],
-): void {
-	if (i >= array.length) return;
-	array[i] = value;
+export function fromArray(array: ArrayLike<number>, i: number): number {
+	return array[i] as number;
 }
