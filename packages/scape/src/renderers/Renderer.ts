e3017d760f57c144092bb3ab983f0e5350d1b23f
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 20:49:43 +0300

    whoops

--- packages/scape/src/renderers/Renderer.ts
+++ packages/scape/src/renderers/Renderer.ts
@@ -1,6 +1,5 @@
 import type { Camera } from "../cameras/Camera.ts";
 import { PerspCamera } from "../cameras/PerspCamera.ts";
-import { Vector4 } from "../maths/Vector4.ts";
 import type { Scene } from "../scenes/Scene.ts";
 
 export interface RendererOptions {
@@ -14,8 +13,6 @@ export class Renderer {
 	height: number;
 	alpha: boolean;
 
-	viewport: Vector4;
-
 	get aspect(): number {
 		return this.width / this.height;
 	}
@@ -24,8 +21,6 @@ export class Renderer {
 		this.width = options.width ?? globalThis.innerWidth;
 		this.height = options.height ?? globalThis.innerHeight;
 		this.alpha = options.alpha ?? false;
-
-		this.viewport = new Vector4(0, 0, this.width, this.height);
 	}
 
 	render(_scene: Scene, camera: Camera): this {
@@ -44,12 +39,7 @@ export class Renderer {
 	setSize(width: number, height: number): this {
 		this.width = width;
 		this.height = height;
-		this.viewport.set(0, 0, width, height);
-		return this;
-	}
 
-	setViewport(x: number, y: number, width: number, height: number): this {
-		this.viewport.set(x, y, width, height);
 		return this;
 	}
 }
