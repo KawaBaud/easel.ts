b72a70fd7fde49c256abeea3662fee48e57a4bd0
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 21:40:29 +0300

    statics, others

--- src/common/Color.ts
+++ src/common/Color.ts
@@ -1,21 +1,35 @@
 export class Color {
+	static rgbToHex(r: number, g: number, b: number): number {
+		return (r << 16) ^ (g << 8) ^ b;
+	}
+
+	static rgbToHsl(r: number, g: number, b: number): number[] {
+		const max = Math.max(r, g, b);
+		const min = Math.min(r, g, b);
+		const l = (max + min) / 2;
+
+		if (max === min) return [0, 0, l];
+
+		const d = max - min;
+		const s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+		let h;
+		if (max === r) {
+			h = (g - b) / d + (g < b ? 6 : 0);
+		} else if (max === g) {
+			h = (b - r) / d + 2;
+		} else {
+			h = (r - g) / d + 4;
+		}
+		h /= 6;
+		return [h, s, l];
+	}
+
 	constructor(
 		public r = 1,
 		public g = 1,
 		public b = 1,
 	) {}
-
-	get hex(): number {
-		return (this.r * 255) << 16 ^ (this.g * 255) << 8 ^ (this.b * 255);
-	}
-
-	get hexString(): string {
-		return this.hex.toString(16).padStart(6, "0");
-	}
-
-	clone(): Color {
-		return new Color(this.r, this.g, this.b);
-	}
 
 	copy(source: Color): Color {
 		this.r = source.r;
@@ -55,6 +69,30 @@ export class Color {
 		return this;
 	}
 
+	setHSL(h: number, s: number, l: number): this {
+		const hue2rgb = (p: number, q: number, t: number): number => {
+			if (t < 0) t += 1;
+			if (t > 1) t -= 1;
+			if (t < 1 / 6) return p + (q - p) * 6 * t;
+			if (t < 1 / 2) return q;
+			if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+			return p;
+		};
+
+		if (s === 0) {
+			this.r = this.g = this.b = l;
+		} else {
+			const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			const p = 2 * l - q;
+
+			this.r = hue2rgb(p, q, h + 1 / 3);
+			this.g = hue2rgb(p, q, h);
+			this.b = hue2rgb(p, q, h - 1 / 3);
+		}
+
+		return this;
+	}
+
 	setRGB(r: number, g: number, b: number): this {
 		if (r > 255 || g > 255 || b > 255) {
 			throw new Error("Color: rgb value out of range");
@@ -74,26 +112,23 @@ export class Color {
 		throw new Error("Color: invalid style");
 	}
 
+	parse(value: string | number | Color): this {
+		if (value instanceof Color) {
+			this.copy(value);
+			return this;
+		} else if (typeof value === "number") {
+			return this.setHex(value);
+		} else if (typeof value === "string") {
+			return this.setStyle(value);
+		}
+		throw new Error("Color: invalid value type");
+	}
+
 	#parseHex(style: string): this {
 		if (style.length !== 7) {
 			throw new Error("Color: hex style must be in '#rrggbb' format");
 		}
 		this.setHex(parseInt(style.slice(1), 16));
-		return this;
-	}
-
-	#parseRGB(style: string): this {
-		const values = style.match(/\d+/g);
-		if (!values || values.length < 3) {
-			throw new Error(
-				"Color: rgb(a) style must be in 'rgb(r,g,b)' or 'rgba(r,g,b,a)' format",
-			);
-		}
-		this.setRGB(
-			Number(values[0]) / 255,
-			Number(values[1]) / 255,
-			Number(values[2]) / 255,
-		);
 		return this;
 	}
 
@@ -123,6 +158,21 @@ export class Color {
 		this.r = hueToRGB(p, q, h + 1 / 3);
 		this.g = hueToRGB(p, q, h);
 		this.b = hueToRGB(p, q, h - 1 / 3);
+		return this;
+	}
+
+	#parseRGB(style: string): this {
+		const values = style.match(/\d+/g);
+		if (!values || values.length < 3) {
+			throw new Error(
+				"Color: rgb(a) style must be in 'rgb(r,g,b)' or 'rgba(r,g,b,a)' format",
+			);
+		}
+		this.setRGB(
+			Number(values[0]) / 255,
+			Number(values[1]) / 255,
+			Number(values[2]) / 255,
+		);
 		return this;
 	}
 }
