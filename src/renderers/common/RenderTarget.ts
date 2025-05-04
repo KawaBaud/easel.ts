285a54bd88280537972daba891a975cd597240cb
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 22:18:12 +0300

    commit

---
+++ src/renderers/common/RenderTarget.ts
@@ -0,0 +1,15 @@
+export class RenderTarget {
+	width: number;
+	height: number;
+	
+	constructor(width?: number, height?: number) {
+		this.width = width ?? globalThis.innerWidth;
+		this.height = height ?? globalThis.innerHeight;
+	}
+	
+	setSize(width: number, height: number): this {
+		this.width = width;
+		this.height = height;
+		return this;
+	}
+}
