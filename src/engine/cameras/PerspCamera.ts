d2ab3fcbe2e627bf13dbe0e7497a2c22afbccbc1
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-04 10:58:45 +0300

    bring back Persp

---
+++ src/engine/cameras/PerspCamera.ts
@@ -0,0 +1,44 @@
+import { MathUtils } from "../maths/MathUtils.ts";
+import { Camera } from "./Camera.ts";
+
+export class PerspCamera extends Camera {
+	readonly isPerspCamera = true;
+
+	constructor(
+		public fov = 50,
+		public aspect = 1,
+		public near = 0.1,
+		public far = 2000,
+	) {
+		super();
+		this.name = "PerspCamera";
+		this.updateProjectionMatrix();
+	}
+
+	override clone(): PerspCamera {
+		return new PerspCamera(
+			this.fov,
+			this.aspect,
+			this.near,
+			this.far,
+		);
+	}
+
+	override copy(source: PerspCamera): this {
+		super.copy(source);
+		this.fov = source.fov;
+		this.aspect = source.aspect;
+		this.near = source.near;
+		this.far = source.far;
+		return this;
+	}
+
+	override updateProjectionMatrix(): void {
+		this.projectionMatrix.makePerspective(
+			MathUtils.toRadians(this.fov),
+			this.aspect,
+			this.near,
+			this.far,
+		);
+	}
+}
