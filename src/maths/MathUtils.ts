91e9530a997af710ba2e80f4c0be6dfdfc9d9b1a
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-05 16:03:20 +0300

    refs

--- src/maths/MathUtils.ts
+++ src/maths/MathUtils.ts
@@ -1,11 +1,19 @@
 export class MathUtils {
 	static readonly EPSILON: number = 1e-4;
 
+	/** 2 * Math.PI */
 	static readonly TAU: number = 6.283185307179586;
+
+	/** Math.PI / 2 */
 	static readonly HALF_PI: number = 1.5707963267948966;
+
+	/** Math.PI / 4 */
 	static readonly QUARTER_PI: number = 0.7853981633974483;
 
+	/** 180 / Math.PI */
 	static readonly RAD2DEG: number = 57.29577951308232;
+
+	/** Math.PI / 180 */
 	static readonly DEG2RAD: number = 0.017453292519943295;
 
 	static clamp(value: number, min: number, max: number): number {
