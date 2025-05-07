991212c253b98ab2c7853b8bded7de97a80762d6
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-08 02:27:13 +0300

    filled triangle support

--- src/renderers/common/Rasterizer.ts
+++ src/renderers/common/Rasterizer.ts
@@ -18,6 +18,7 @@ export class Rasterizer {
 	ctx: CanvasRenderingContext2D;
 	imageData: ImageData;
 	data: Uint8ClampedArray;
+
 	#tempColor = new Color(0, 0, 0);
 
 	constructor(options: RasterizerOptions = {}) {
@@ -131,12 +132,38 @@ export class Rasterizer {
 		color: ColorValue,
 		wireframe = false,
 	): this {
-		if (wireframe) {
-			this.drawLine(v1, v2, color);
-			this.drawLine(v2, v3, color);
-			this.drawLine(v3, v1, color);
-			return this;
-		}
+		wireframe
+			? (
+				this.drawLine(v1, v2, color),
+					this.drawLine(v2, v3, color),
+					this.drawLine(v3, v1, color)
+			)
+			: this.drawTriangleFilled(v1, v2, v3, color);
+		return this;
+	}
+
+	drawTriangleFilled(
+		v1: Vector3,
+		v2: Vector3,
+		v3: Vector3,
+		color: ColorValue,
+	): this {
+		let p1 = this.#projectToScreen(v1);
+		let p2 = this.#projectToScreen(v2);
+		let p3 = this.#projectToScreen(v3);
+		[p1, p2, p3] = this.#sortVerticesByY(p1, p2, p3);
+
+		p1.y === p2.y
+			? this.#drawFlatTopTriangle(p1, p2, p3, color)
+			: p2.y === p3.y
+			? this.#drawFlatBottomTriangle(p1, p2, p3, color)
+			: (() => {
+				const p4x = p1.x + ((p2.y - p1.y) / (p3.y - p1.y)) * (p3.x - p1.x);
+				const p4 = new Vector3(p4x, p2.y, 0);
+
+				this.#drawFlatBottomTriangle(p1, p2, p4, color);
+				this.#drawFlatTopTriangle(p2, p4, p3, color);
+			})();
 		return this;
 	}
 
@@ -203,9 +230,89 @@ export class Rasterizer {
 		return this;
 	}
 
-	#projectToScreen(point: Vector3): Vector3 {
-		const x = MathUtils.fastTrunc((point.x + 1) * this.width / 2);
-		const y = MathUtils.fastTrunc((-point.y + 1) * this.height / 2);
-		return new Vector3(x, y, point.z);
+	#drawFlatTopTriangle(
+		p1: Vector3,
+		p2: Vector3,
+		p3: Vector3,
+		color: ColorValue,
+	): void {
+		const { x: x1, y: y1 } = p1;
+		const { x: x2, y: y2 } = p2;
+		const { x: x3, y: y3 } = p3;
+
+		const dP1P3 = (x3 - x1) / (y3 - y1);
+		const dP2P3 = (x3 - x2) / (y3 - y2);
+
+		for (
+			let y = MathUtils.clamp(Math.ceil(y1), 0, this.height - 1);
+			y <= MathUtils.clamp(Math.floor(y3), 0, this.height - 1);
+			y++
+		) {
+			const sx = x1 + (y - y1) * dP1P3;
+			const ex = x2 + (y - y2) * dP2P3;
+			this.#scanline(y, sx, ex, color);
+		}
+	}
+
+	#drawFlatBottomTriangle(
+		p1: Vector3,
+		p2: Vector3,
+		p3: Vector3,
+		color: ColorValue,
+	): void {
+		const { x: x1, y: y1 } = p1;
+		const { x: x2, y: y2 } = p2;
+		const { x: x3, y: y3 } = p3;
+
+		const dP1P2 = (x2 - x1) / (y2 - y1);
+		const dP1P3 = (x3 - x1) / (y3 - y1);
+
+		for (
+			let y = MathUtils.clamp(Math.ceil(y1), 0, this.height - 1);
+			y <= MathUtils.clamp(Math.floor(y2), 0, this.height - 1);
+			y++
+		) {
+			const sx = x1 + (y - y1) * dP1P2;
+			const ex = x1 + (y - y1) * dP1P3;
+			this.#scanline(y, sx, ex, color);
+		}
+	}
+
+	#projectToScreen(vertex: Vector3): Vector3 {
+		const screenX = MathUtils.fastRound((vertex.x + 1) * 0.5 * this.width);
+		const screenY = MathUtils.fastRound((1 - vertex.y) * 0.5 * this.height);
+		return new Vector3(screenX, screenY, vertex.z);
+	}
+
+	#scanline(
+		y: number,
+		x1: number,
+		x2: number,
+		color: ColorValue,
+	): void {
+		const startX = MathUtils.clamp(
+			Math.ceil(Math.min(x1, x2)),
+			0,
+			this.width - 1,
+		);
+		const endX = MathUtils.clamp(
+			Math.floor(Math.max(x1, x2)),
+			0,
+			this.width - 1,
+		);
+		for (let x = startX; x <= endX; x++) {
+			this.setPixel(x, y, color);
+		}
+	}
+
+	#sortVerticesByY(
+		p1: Vector3,
+		p2: Vector3,
+		p3: Vector3,
+	): [Vector3, Vector3, Vector3] {
+		if (p1.y > p2.y) [p1, p2] = [p2, p1];
+		if (p1.y > p3.y) [p1, p3] = [p3, p1];
+		if (p2.y > p3.y) [p2, p3] = [p3, p2];
+		return [p1, p2, p3];
 	}
 }
