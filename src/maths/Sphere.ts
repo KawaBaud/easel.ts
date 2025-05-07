b4938626cd932c0fa77b19f907be5b5bbb110cca
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-08 00:24:53 +0300

    all pass

---
+++ src/maths/Sphere.ts
@@ -0,0 +1,66 @@
+import "../types.ts";
+import { Vector3 } from "./Vector3.ts";
+
+export class Sphere {
+	#centre = new Vector3();
+	#radius = 1;
+
+	constructor(centre = new Vector3(), radius = 1) {
+		this.#centre = centre.clone();
+		this.#radius = radius;
+	}
+
+	get centre(): Vector3 {
+		return this.#centre;
+	}
+
+	set centre(value: Vector3) {
+		this.#centre.copy(value);
+	}
+
+	get radius(): number {
+		return this.#radius;
+	}
+
+	set radius(value: number) {
+		this.#radius = value;
+	}
+
+	clone(): Sphere {
+		return new Sphere(this.centre, this.radius);
+	}
+
+	containsPoint(point: Vector3): boolean {
+		return point.clone().sub(this.centre).lengthSq <= this.radius * this.radius;
+	}
+
+	copy(sphere: Sphere): this {
+		this.centre.copy(sphere.centre);
+		this.radius = sphere.radius;
+		return this;
+	}
+
+	distanceToPoint(point: Vector3): number {
+		return point.clone().sub(this.centre).length - this.radius;
+	}
+
+	equals(sphere: Sphere): boolean {
+		return (
+			(sphere.centre.x === this.centre.x) &&
+			(sphere.centre.y === this.centre.y) &&
+			(sphere.centre.z === this.centre.z) &&
+			(sphere.radius === this.radius)
+		);
+	}
+
+	intersectsSphere(sphere: Sphere): boolean {
+		const radiusSum = this.radius + sphere.radius;
+		return this.centre.clone().sub(sphere.centre).lengthSq <=
+			radiusSum * radiusSum;
+	}
+
+	translate(offset: Vector3): this {
+		this.centre.add(offset);
+		return this;
+	}
+}
---
+++ tests/maths/Sphere.test.ts
@@ -0,0 +1,310 @@
+import {
+	assertAlmostEquals,
+	assertEquals,
+	assertNotStrictEquals,
+} from "@std/assert";
+import { Sphere as ThreeSphere, Vector3 as ThreeVector3 } from "three";
+import { MathUtils } from "../../src/maths/MathUtils.ts";
+import { Sphere } from "../../src/maths/Sphere.ts";
+import { Vector3 } from "../../src/maths/Vector3.ts";
+import "../../src/types.ts";
+
+function compareSpheres(
+	ourSphere: Sphere,
+	threeSphere: ThreeSphere,
+	message: string,
+): void {
+	console.log(`${message}:`);
+	console.log(
+		`  Our:   centre(${ourSphere.centre.x}, ${ourSphere.centre.y}, ${ourSphere.centre.z}), radius=${ourSphere.radius}`,
+	);
+	console.log(
+		`  Three: center(${threeSphere.center.x}, ${threeSphere.center.y}, ${threeSphere.center.z}), radius=${threeSphere.radius}`,
+	);
+
+	assertAlmostEquals(
+		ourSphere.centre.x,
+		threeSphere.center.x,
+		MathUtils.EPSILON,
+		`${message} (centre.x)`,
+	);
+	assertAlmostEquals(
+		ourSphere.centre.y,
+		threeSphere.center.y,
+		MathUtils.EPSILON,
+		`${message} (centre.y)`,
+	);
+	assertAlmostEquals(
+		ourSphere.centre.z,
+		threeSphere.center.z,
+		MathUtils.EPSILON,
+		`${message} (centre.z)`,
+	);
+	assertAlmostEquals(
+		ourSphere.radius,
+		threeSphere.radius,
+		MathUtils.EPSILON,
+		`${message} (radius)`,
+	);
+}
+
+Deno.test("Sphere: constructor", () => {
+	const a = new Sphere();
+	assertEquals(a.centre.x, 0);
+	assertEquals(a.centre.y, 0);
+	assertEquals(a.centre.z, 0);
+	assertEquals(a.radius, 1);
+
+	const centre = new Vector3(1, 2, 3);
+	const radius = 5;
+	const b = new Sphere(centre, radius);
+	const threeB = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+	assertAlmostEquals(
+		b.centre.x,
+		threeB.center.x,
+		MathUtils.EPSILON,
+		"constructor w/ values (centre.x)",
+	);
+	assertAlmostEquals(
+		b.centre.y,
+		threeB.center.y,
+		MathUtils.EPSILON,
+		"constructor w/ values (centre.y)",
+	);
+	assertAlmostEquals(
+		b.centre.z,
+		threeB.center.z,
+		MathUtils.EPSILON,
+		"constructor w/ values (centre.z)",
+	);
+	assertAlmostEquals(
+		b.radius,
+		threeB.radius,
+		MathUtils.EPSILON,
+		"constructor w/ values (radius)",
+	);
+});
+
+Deno.test("Sphere: clone", () => {
+	const a = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const b = a.clone();
+	const threeA = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+	const threeB = threeA.clone();
+
+	compareSpheres(b, threeB, "clone");
+	assertNotStrictEquals(a, b, "clone creates new instance");
+});
+
+Deno.test("Sphere: containsPoint", () => {
+	const a = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const threeA = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+
+	const point1 = new Vector3(2, 3, 4);
+	const threePoint1 = new ThreeVector3(2, 3, 4);
+	assertEquals(
+		a.containsPoint(point1),
+		threeA.containsPoint(threePoint1),
+		"containsPoint (inside)",
+	);
+
+	const point2 = new Vector3(1, 2, 8);
+	const threePoint2 = new ThreeVector3(1, 2, 8);
+	assertEquals(
+		a.containsPoint(point2),
+		threeA.containsPoint(threePoint2),
+		"containsPoint (on surface)",
+	);
+
+	const point3 = new Vector3(10, 10, 10);
+	const threePoint3 = new ThreeVector3(10, 10, 10);
+	assertEquals(
+		a.containsPoint(point3),
+		threeA.containsPoint(threePoint3),
+		"containsPoint (outside)",
+	);
+});
+
+Deno.test("Sphere: copy", () => {
+	const a = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const b = new Sphere().copy(a);
+	const threeA = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+	const threeB = new ThreeSphere().copy(threeA);
+	compareSpheres(b, threeB, "copy");
+
+	a.centre.set(0, 0, 0);
+	a.radius = 1;
+	threeA.center.set(0, 0, 0);
+	threeA.radius = 1;
+	compareSpheres(b, threeB, "copy (after modifying original)");
+});
+
+Deno.test("Sphere: distanceToPoint", () => {
+	const a = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const threeA = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+
+	const point1 = new Vector3(2, 3, 4);
+	const threePoint1 = new ThreeVector3(2, 3, 4);
+	assertAlmostEquals(
+		a.distanceToPoint(point1),
+		threeA.distanceToPoint(threePoint1),
+		MathUtils.EPSILON,
+		"distanceToPoint (inside)",
+	);
+
+	const point2 = new Vector3(1, 2, 8);
+	const threePoint2 = new ThreeVector3(1, 2, 8);
+	assertAlmostEquals(
+		a.distanceToPoint(point2),
+		threeA.distanceToPoint(threePoint2),
+		MathUtils.EPSILON,
+		"distanceToPoint (on surface)",
+	);
+
+	const point3 = new Vector3(10, 10, 10);
+	const threePoint3 = new ThreeVector3(10, 10, 10);
+	assertAlmostEquals(
+		a.distanceToPoint(point3),
+		threeA.distanceToPoint(threePoint3),
+		MathUtils.EPSILON,
+		"distanceToPoint (outside)",
+	);
+});
+
+Deno.test("Sphere: equals", () => {
+	const a = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const b = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const c = new Sphere(
+		new Vector3(1, 2, 3),
+		6,
+	);
+	const d = new Sphere(
+		new Vector3(4, 5, 6),
+		5,
+	);
+
+	const threeA = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+	const threeB = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+	const threeC = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		6,
+	);
+	const threeD = new ThreeSphere(
+		new ThreeVector3(4, 5, 6),
+		5,
+	);
+
+	assertEquals(a.equals(b), threeA.equals(threeB), "equals (true)");
+	assertEquals(a.equals(c), threeA.equals(threeC), "equals (different radius)");
+	assertEquals(a.equals(d), threeA.equals(threeD), "equals (different centre)");
+});
+
+Deno.test("Sphere: intersectsSphere", () => {
+	const a = new Sphere(
+		new Vector3(0, 0, 0),
+		5,
+	);
+	const threeA = new ThreeSphere(
+		new ThreeVector3(0, 0, 0),
+		5,
+	);
+
+	const b = new Sphere(
+		new Vector3(8, 0, 0),
+		5,
+	);
+	const threeB = new ThreeSphere(
+		new ThreeVector3(8, 0, 0),
+		5,
+	);
+	assertEquals(
+		a.intersectsSphere(b),
+		threeA.intersectsSphere(threeB),
+		"intersectsSphere (intersect)",
+	);
+
+	const c = new Sphere(
+		new Vector3(10, 0, 0),
+		5,
+	);
+	const threeC = new ThreeSphere(
+		new ThreeVector3(10, 0, 0),
+		5,
+	);
+	assertEquals(
+		a.intersectsSphere(c),
+		threeA.intersectsSphere(threeC),
+		"intersectsSphere (touch)",
+	);
+
+	const d = new Sphere(
+		new Vector3(15, 0, 0),
+		5,
+	);
+	const threeD = new ThreeSphere(
+		new ThreeVector3(15, 0, 0),
+		5,
+	);
+	assertEquals(
+		a.intersectsSphere(d),
+		threeA.intersectsSphere(threeD),
+		"intersectsSphere (no intersection)",
+	);
+});
+
+Deno.test("Sphere: translate", () => {
+	const a = new Sphere(
+		new Vector3(1, 2, 3),
+		5,
+	);
+	const threeA = new ThreeSphere(
+		new ThreeVector3(1, 2, 3),
+		5,
+	);
+
+	const offset = new Vector3(10, 20, 30);
+	const threeOffset = new ThreeVector3(10, 20, 30);
+
+	a.translate(offset);
+	threeA.translate(threeOffset);
+
+	compareSpheres(a, threeA, "translate");
+});
