5dce6650503204d0e714e58eff17ec3492bdd562
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-07 16:26:17 +0300

    add fastAtan2

--- src/maths/MathUtils.ts
+++ src/maths/MathUtils.ts
@@ -16,6 +16,26 @@ export class MathUtils {
 	/** Math.PI / 180 */
 	static readonly DEG2RAD: number = 0.017453292519943295;
 
+	static fastAtan2(y: number, x: number): number {
+		if (x === 0) return y > 0 ? MathUtils.HALF_PI : -MathUtils.HALF_PI;
+		if (y === 0) return x >= 0 ? 0 : Math.PI;
+
+		const absY = Math.abs(y);
+		const absX = Math.abs(x);
+
+		const inOctant = absY <= absX;
+
+		const a = inOctant ? absY / absX : absX / absY;
+		const s = a * a;
+		let r = ((-0.046496 * s + 0.15931) * s - 0.32763) * s * a + a;
+
+		if (!inOctant) r = MathUtils.HALF_PI - r;
+		if (x < 0) r = Math.PI - r;
+		if (y < 0) r = -r;
+
+		return r;
+	}
+
 	static clamp(value: number, min: number, max: number): number {
 		return Math.max(min, Math.min(value, max));
 	}
