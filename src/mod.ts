cb64cc423277a62f4d5f4b3f10e875524472edb0
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-09 22:51:39 +0300

    more exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,18 @@
 import { OrthoCamera } from "./cameras/OrthoCamera.ts";
 import { PerspCamera } from "./cameras/PerspCamera.ts";
 import { Color } from "./common/Color.ts";
+import { OBJLoader } from "./loaders/OBJLoader.ts";
 import { Material } from "./materials/Material.ts";
+import { Box3 } from "./maths/Box3.ts";
 import { Euler } from "./maths/Euler.ts";
+import { Frustum } from "./maths/Frustum.ts";
+import { Line3 } from "./maths/Line3.ts";
 import { MathUtils } from "./maths/MathUtils.ts";
+import { Matrix3 } from "./maths/Matrix3.ts";
 import { Matrix4 } from "./maths/Matrix4.ts";
+import { Plane } from "./maths/Plane.ts";
 import { Quaternion } from "./maths/Quaternion.ts";
+import { Sphere } from "./maths/Sphere.ts";
 import { Vector2 } from "./maths/Vector2.ts";
 import { Vector3 } from "./maths/Vector3.ts";
 import { Vector4 } from "./maths/Vector4.ts";
@@ -23,14 +30,23 @@ export const EASEL = {
 	/* common */
 	Color,
 
+	/* loaders */
+	OBJLoader,
+
 	/* materials */
 	Material,
 
 	/* maths */
+	Box3,
 	Euler,
+	Frustum,
+	Line3,
 	MathUtils,
+	Matrix3,
 	Matrix4,
+	Plane,
 	Quaternion,
+	Sphere,
 	Vector2,
 	Vector3,
 	Vector4,
