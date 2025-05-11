05d88b746c61316d2fd3711f2301c5b60efeb4e9
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-11 17:22:33 +0300

    base loader

---
+++ src/loaders/Loader.ts
@@ -0,0 +1,27 @@
+import type { Object3D } from "../objects/Object3D.ts";
+
+export abstract class Loader<T extends Object3D = Object3D> {
+	load(
+		url: string,
+		onLoad: (data: T) => void,
+		onProgress?: (event: ProgressEvent) => void,
+		onError?: (err: unknown) => void,
+	): void {
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`unable to load file: ${response.statusText}`);
+				}
+				return response.text();
+			})
+			.then((text) => {
+				onLoad(this.parse(text));
+			})
+			.catch(onError)
+			.finally(() => {
+				onProgress?.(new ProgressEvent("load"));
+			});
+	}
+
+	abstract parse(text: string): T;
+}
