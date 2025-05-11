758e05367c442a935ab226927c0e374731e4fa13
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-11 14:57:50 +0300

    static dotProduct

--- src/maths/Vector3.ts
+++ src/maths/Vector3.ts
@@ -10,6 +10,10 @@ const _q = new Quaternion();
 export class Vector3 {
 	static UP = new Vector3(0, 1, 0);
 
+	static dotComponents(x: number, y: number, z: number, v: Vector3): number {
+		return (x * v.x) + (y * v.y) + (z * v.z);
+	}
+
 	#x = 0;
 	#y = 0;
 	#z = 0;
