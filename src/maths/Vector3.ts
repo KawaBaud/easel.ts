8eb652b97bddd231329bb12792581f0bac1a9223
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-11 18:34:12 +0300

    add DOWN

--- src/maths/Vector3.ts
+++ src/maths/Vector3.ts
@@ -9,6 +9,7 @@ const _q = new Quaternion();
 
 export class Vector3 {
 	static UP = new Vector3(0, 1, 0);
+	static DOWN = new Vector3(0, -1, 0);
 
 	static dotComponents(x: number, y: number, z: number, v: Vector3): number {
 		return (x * v.x) + (y * v.y) + (z * v.z);
