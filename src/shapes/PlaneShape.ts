c2b6f2a30b7d147d694d5c7665f664af107e8938
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-13 21:05:03 +0300

    match three.js

--- src/shapes/PlaneShape.ts
+++ src/shapes/PlaneShape.ts
@@ -2,23 +2,36 @@ import { Vector3 } from "../maths/Vector3.ts";
 import { Shape } from "./Shape.ts";
 
 export class PlaneShape extends Shape {
-	constructor(width = 1, height = 1) {
+	constructor(width = 1, height = 1, widthSegments = 1, heightSegments = 1) {
 		super();
 
-		const halfWidth = width / 2;
-		const halfHeight = height / 2;
+		const gridX = Math.floor(widthSegments);
+		const gridY = Math.floor(heightSegments);
+		const gridX1 = gridX + 1;
+		const gridY1 = gridY + 1;
 
-		this.vertices = [
-			new Vector3(-halfWidth, 0, -halfHeight),
-			new Vector3(halfWidth, 0, -halfHeight),
-			new Vector3(halfWidth, 0, halfHeight),
-			new Vector3(-halfWidth, 0, halfHeight),
-		];
+		const segmentWidth = width / gridX;
+		const segmentHeight = height / gridY;
 
-		const triangleA = [0, 1, 2];
-		const triangleB = [2, 3, 0];
+		for (let iy = 0; iy < gridY1; iy++) {
+			const y = iy * segmentHeight - height / 2;
+			for (let ix = 0; ix < gridX1; ix++) {
+				const x = ix * segmentWidth - width / 2;
+				this.vertices.push(new Vector3(x, y, 0));
+			}
+		}
 
-		this.indices = [...triangleA, ...triangleB];
+		for (let iy = 0; iy < gridY; iy++) {
+			for (let ix = 0; ix < gridX; ix++) {
+				const a = ix + gridX1 * iy;
+				const b = ix + gridX1 * (iy + 1);
+				const c = (ix + 1) + gridX1 * (iy + 1);
+				const d = (ix + 1) + gridX1 * iy;
+
+				this.indices.push(a, b, d);
+				this.indices.push(b, c, d);
+			}
+		}
 	}
 
 	override clone(): PlaneShape {
