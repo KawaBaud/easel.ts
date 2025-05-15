129f005313e686611ae579b8078293f10698223f
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-16 01:20:46 +0300

    relocate

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,14 @@
-export {
-	type ColorValue,
-	type HSL,
-	type HSLArray,
-	type RGB,
-	type RGBA,
-	type RGBAArray,
-	type RGBArray,
-} from "./types/color.types.ts";
+import type { Color } from "./common/Color.ts";
+
+export type ColorValue = string | number | Color;
+
+export type HSL = { h: number; s: number; l: number };
+export type RGB = { r: number; g: number; b: number };
+export type RGBA = RGB & { a: number };
+
+export type HSLArray = [h: number, s: number, l: number];
+export type RGBArray = [r: number, g: number, b: number];
+export type RGBAArray = [r: number, g: number, b: number, a: number];
 
 /* const enum */
 export const Side = {
