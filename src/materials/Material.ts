fd2b2fe14d2956a418cc5a7757dd9a9af248e33f
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-05-16 00:13:33 +0300

    default Material base

--- src/materials/Material.ts
+++ src/materials/Material.ts
@@ -1,22 +1,25 @@
-import type { ColorType } from "../common/Color.ts";
-import { Side } from "../consts.ts";
+import { type ColorValue, Side } from "../types.ts";
 
 export interface MaterialOptions {
-	color?: ColorType;
+	color?: ColorValue;
 	opacity?: number;
 	dithering?: boolean;
 	side?: Side;
 	vertexColors?: boolean;
 	visible?: boolean;
+	fog?: boolean;
+	wireframe?: boolean;
 }
 
 export class Material {
-	color: ColorType;
+	color: ColorValue;
 	opacity: number;
 	dithering: boolean;
 	side: Side;
 	vertexColors: boolean;
 	visible: boolean;
+	fog: boolean;
+	wireframe: boolean;
 
 	constructor(options: MaterialOptions = {}) {
 		this.color = options.color ?? 0xFFFFFF;
@@ -25,6 +28,8 @@ export class Material {
 		this.side = options.side ?? Side.FRONT;
 		this.vertexColors = options.vertexColors ?? false;
 		this.visible = options.visible ?? true;
+		this.fog = options.fog ?? true;
+		this.wireframe = options.wireframe ?? false;
 	}
 
 	clone(): Material {
@@ -38,6 +43,8 @@ export class Material {
 		this.side = source.side;
 		this.vertexColors = source.vertexColors;
 		this.visible = source.visible;
+		this.fog = source.fog;
+		this.wireframe = source.wireframe;
 		return this;
 	}
 }
